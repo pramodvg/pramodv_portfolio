6cd17db66ad7c23f7fd5724ac9eb3bf93ddac75d
Pramod Vaghasia <pridetoracer@gmail.com>
2022-11-14 18:54:12 -0500

    front end

--- src/Component/Layouts/Footer/footer.js
+++ src/Component/Layouts/Footer/footer.js
@@ -1,5 +1,12 @@
 import React from 'react'
-
+// import Toolbar from './Component/Navigation/Toolbar/Toolbar'
+// import Home from './Component/Layouts/Home/home';
+// import About from './Component/Layouts/About/about'
+// import Service from './Component/Layouts/Service/service'
+// import Portfolio from './Component/Layouts/Portfolio/Portfolio'
+// import ContactMe from './Component/Layouts/Contact/Contactme'
+// import Footer from './Component/Layouts/Footer/footer'
+// import { Routes, Route } from "react-router-dom";
 export default function footer() {
     return (
         <div>
@@ -15,6 +22,16 @@ export default function footer() {
                     Copyright @ 2022 All Rights Reserved By <strong>Pramod Vaghasia.</strong>
                 </p>
             </footer>
+            {/* <Toolbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/service" element={<Service />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<ContactMe />} />
+                <Route path="*" element={<Footer />} />
+            </Routes>
+            <Footer /> */}
         </div>
     )
 }
